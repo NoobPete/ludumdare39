9497e89ab66750ab0d42439008452f7cb6200a27
Noob Pete <peteretep@live.dk>
2017-07-29 17:14:12 +0200

    Camera lifted a bit up

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour {
     public GameObject player1;
     public GameObject player2;
-    public int distance;
+    public float distance;
+    public float upDown;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +20,7 @@ public class CameraScript : MonoBehaviour {
             transform.position = player1.transform.position + (player2.transform.position - player1.transform.position) / 2 + new Vector3(0, 0, distance);
         } else
         {
-            transform.position = player1.transform.position + new Vector3(0, 0, distance);
+            transform.position = player1.transform.position + new Vector3(0, upDown, distance);
         }
 	}
 }
