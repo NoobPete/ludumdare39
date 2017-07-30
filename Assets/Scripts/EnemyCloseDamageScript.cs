1b133cc51c8259386f2366b090807de78b517c2d
Lars Lundqvist <lars_lundqvist@hotmail.com>
2017-07-30 11:19:23 +0200

    Fixed singleplayer bug and started impl. enemy melee dmg

---
+++ Assets/Scripts/EnemyCloseDamageScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCloseDamageScript : MonoBehaviour {
+    public float timeMeasurement = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeMeasurement += Time.deltaTime;
+
+        List<PlayerController> alreadyHit = new List<PlayerController>();
+        foreach (RaycastHit2D rc in Physics2D.BoxCastAll(transform.position, new Vector2(2.2f, 4.4f), 0f, new Vector2(0, 0)))
+        {
+            PlayerController player = rc.transform.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                if (!alreadyHit.Contains(player))
+                {
+                    alreadyHit.Add(player);
+                    player.Damage(20);
+                }
+            }
+        }
+    }
+}
