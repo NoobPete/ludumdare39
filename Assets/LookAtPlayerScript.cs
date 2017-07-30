3c7b986cda40e7bf218861b76b597cbad30434c2
Noob Pete <peteretep@live.dk>
2017-07-30 14:32:19 +0200

    Look at player for shooting enemy and main menu battery theme

---
+++ Assets/LookAtPlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayerScript : MonoBehaviour {
+    private bool facingRight;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject o = GameControllerScript.main.GetClosestPlayer(transform);
+        
+        if (o.transform.position.x > transform.position.x && facingRight)
+        {
+            Flip();
+        }
+
+        if (o.transform.position.x < transform.position.x && !facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
